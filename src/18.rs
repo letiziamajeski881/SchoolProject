1c9679f7c73e5747a10cc890855b8cce676f97e3
letiziamajeski881 <letiziamajeski881@gmail.com>
2025-03-23 10:20:11 +0000

    Add 18.rs

---
+++ src/18.rs
@@ -0,0 +1,12 @@
+use std::cmp;
+
+fn main() {
+    let x = 5;
+    let y = 10;
+
+    if x > y {
+        println!("x is greater than y");
+    } else {
+        println!("y is greater than x");
+    }
+}
